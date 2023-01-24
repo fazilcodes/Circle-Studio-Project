c21b77f8526f45d2a4e988f23f0f19a8c66a98c6
Fazil <122675114+fazilcodes@users.noreply.github.com>
2023-01-24 19:32:28 +0530

    added Profile-model

--- Circle_Studio/urls.py
+++ Circle_Studio/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import main.urls
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main.urls))
 ]
+
+
+urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
